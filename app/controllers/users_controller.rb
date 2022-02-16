f000b714681b833f338356314b03652100943bf2
Ademola <ademolaogunmokun@yahoo.com>
2022-02-17 00:20:24 +0100

    paste to edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,15 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
   private
 
   def user_params
