e1515a3a8146f9ad73d838086a7dbb9c6d03a34d
Ademola <ademolaogunmokun@yahoo.com>
2022-02-16 13:02:45 +0100

    new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
   private
-  
+
   def user_params
     params.require(:user).permit('username', 'email', 'password')
   end
-
 end
