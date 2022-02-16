ff2dda76e323cc6e2a90ed1e83486b161f6dafc9
Ademola <ademolaogunmokun@yahoo.com>
2022-02-16 12:26:25 +0100

    new and create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
   end
+  private
+  
+  def user_params
+    params.require(:user).permit('username', 'email', 'password')
+  end
+
 end
