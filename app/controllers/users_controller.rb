c89764b5b39f3aaf74eb0aec077463e16a741477
Ademola <ademolaogunmokun@yahoo.com>
2022-02-21 16:11:54 +0100

    new_user_path in update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to edit_user_path
+      redirect_to new_user_path
     else
       render :edit, status: :unprocessable_entity
     end
